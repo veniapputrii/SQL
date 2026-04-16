d6766d3fc7e723106a4924ce32e70243fbde7172
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-04-16 07:48:11 +0700

    Add files via upload

--- beginner.sql
+++ beginner.sql
@@ -1,1 +1,6 @@
-select * from employee where gender = 'Male' and occupation like 'S__'
+select first_name, last_name, salary,
+case when salary >= 10000 then 'Grade A'
+when salary >= 5000 then 'Grade B'
+when salary >= 1000 then 'Grade C'
+else 'Grade D' end as salary_grade
+from employee
