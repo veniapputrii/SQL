3eeb59f1f7bfc4aa70095ae981421bc8d4e283c1
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-05-22 23:36:44 +0700

    Implement running total for orders
    Add running total calculation for orders by date.

--- beginner.sql
+++ beginner.sql
@@ -156,5 +156,8 @@ https://github.com/veniapputrii/SQL/edit/main/beginner.sql
 --Select first_name, last_name, salary, department, (select avg(salary)
  -- from employee e2 where e2.department = e1.department) as avg_salary from employee e1 order by department.
 
-Select order_id, order_date, quantity, unit_price,total,
-(select max(order_id) from orders o2 where o2.order_id < o1.order_id) previous_order_id from orders o1 order by order_id
+--Select order_id, order_date, quantity, unit_price,total,
+--(select max(order_id) from orders o2 where o2.order_id < o1.order_id) previous_order_id from orders o1 order by order_id
+
+Select order_date, sum(total) total, (select sum(total) from orders where order_date <=o1.order_date) running_total
+from orders o1 group by order_date order by order_date
