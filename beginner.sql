b5d2bb864eeaaab724c1aba14738625add676878
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-06-03 21:28:04 +0700

    Uncomment SQL queries and add new select statement

--- beginner.sql
+++ beginner.sql
@@ -212,6 +212,8 @@ https://github.com/veniapputrii/SQL/edit/main/beginner.sql
 --from restaurants
 
 --DML (Data manipulation language)
-INSERT INTO EMployee(id, first_name, last_name)
- VALUES (1001, 'John', 'Doe');
- 
+--INSERT INTO EMployee(id, first_name, last_name)
+-- VALUES (1001, 'John', 'Doe');
+
+select into employee( id, name, first_name, last_name, gender, email, phone, occupation, salary, marital_status, dob, department)
+select id, name, first_name, last_name, gender, email, phone, occupation, salary, marital_status, dob, department, from temp_employee
